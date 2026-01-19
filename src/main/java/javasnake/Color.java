1030dc629d0a9357874733bd6dbad687455ef115
ZiedYousfi <yousfi_zied@yahoo.com>
2026-01-19 17:37:40 +0100

    Add rendering logic for game grid cells in the Game class

--- src/main/java/javasnake/Color.java
+++ src/main/java/javasnake/Color.java
@@ -4,6 +4,7 @@ public record Color(int r, int g, int b, int a) {
   public static final Color GREEN = new Color(0, 255, 0, 255);
   public static final Color BLUE = new Color(0, 0, 255, 255);
   public static final Color YELLOW = new Color(255, 255, 0, 255);
+  public static final Color RED = new Color(255, 0, 0, 255);
   public static final Color CYAN = new Color(0, 255, 255, 255);
   public static final Color MAGENTA = new Color(255, 0, 255, 255);
   public static final Color BLACK = new Color(0, 0, 0, 255);
--- src/main/java/javasnake/Game.java
+++ src/main/java/javasnake/Game.java
@@ -12,6 +12,9 @@ public class Game {
     this.frameCount = 0;
     this.score = 0;
     this.grid = new GameGrid(20, 20, 32);
+    this.isRunning = true;
+
+    this.render();
   }
 
   public void start() {
@@ -27,5 +30,30 @@ public class Game {
 
   public int getScore() {
     return score;
+  }
+
+  public GameGrid getGrid() {
+    return grid;
+  }
+
+  public void stop() {
+    isRunning = false;
+  }
+
+  public void render() {
+    renderer.clear();
+    for (int r = 0; r < grid.getRows(); r++) {
+      for (int c = 0; c < grid.getCols(); c++) {
+        Cell cell = grid.getCell(r, c);
+        switch (cell.getType()) {
+          case EMPTY -> renderer.setColor(Color.BLACK);
+          case SNAKE -> renderer.setColor(Color.GREEN);
+          case FOOD -> renderer.setColor(Color.RED);
+        }
+        Rect cellRect = new Rect(c * grid.getCellSize(), r * grid.getCellSize(), grid.getCellSize(), grid.getCellSize());
+        renderer.fillRect(cellRect);
+      }
+    }
+    renderer.present();
   }
 }
