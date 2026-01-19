51582040c5bc726f082f4b82cf7dbfb5c31de008
ZiedYousfi <yousfi_zied@yahoo.com>
2026-01-19 18:11:07 +0100

    Refactor Player class to use List for snake body positions and simplify growth logic

--- src/main/java/javasnake/Player.java
+++ src/main/java/javasnake/Player.java
@@ -1,39 +1,39 @@
 package javasnake;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Player {
-  // Head position is always the first element in the array then follows the body positions (from neck to tail)
-  private Cell.Position[] snakeBodyPositions;
+  // Head position is always the first element in the list, then follows the body
+  // positions (from neck to tail)
+  private List<Cell.Position> snakeBodyPositions;
   private Direction currentDirection;
 
   public Player(Cell.Position initialPosition) {
-    this.snakeBodyPositions = new Cell.Position[] { initialPosition };
+    this.snakeBodyPositions = new LinkedList<>();
+    this.snakeBodyPositions.add(initialPosition);
     this.currentDirection = new Direction(1, 0);
   }
 
-  public Cell.Position[] getSnakeBodyPositions() {
+  public List<Cell.Position> getSnakeBodyPositions() {
     return snakeBodyPositions;
   }
 
-  public void changeHeadPosition(Cell.Position newHeadPosition) {
-    Cell.Position[] newPositions = new Cell.Position[snakeBodyPositions.length];
-    newPositions[0] = newHeadPosition;
-    System.arraycopy(snakeBodyPositions, 0, newPositions, 1, snakeBodyPositions.length - 1);
-    this.snakeBodyPositions = newPositions;
-  }
-
-  public void growSnake(Cell.Position newHeadPosition) {
-    Cell.Position[] newPositions = new Cell.Position[snakeBodyPositions.length + 1];
-    newPositions[0] = newHeadPosition;
-    System.arraycopy(snakeBodyPositions, 0, newPositions, 1, snakeBodyPositions.length);
-    this.snakeBodyPositions = newPositions;
-  }
-
-  public void stepUpdate() {
-    Cell.Position currentHead = snakeBodyPositions[0];
+  public void grow() {
+    Cell.Position currentHead = snakeBodyPositions.get(0);
     Cell.Position newHead = new Cell.Position(
         currentHead.row() + currentDirection.deltaX,
         currentHead.col() + currentDirection.deltaY);
-    changeHeadPosition(newHead);
+    snakeBodyPositions.add(0, newHead); // Add new head
+  }
+
+  public void stepUpdate() {
+    Cell.Position currentHead = snakeBodyPositions.get(0);
+    Cell.Position newHead = new Cell.Position(
+        currentHead.row() + currentDirection.deltaX,
+        currentHead.col() + currentDirection.deltaY);
+    snakeBodyPositions.add(0, newHead); // Add new head
+    snakeBodyPositions.remove(snakeBodyPositions.size() - 1); // Remove tail
   }
 
   public record Direction(int deltaX, int deltaY) {
